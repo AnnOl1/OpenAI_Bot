a6b25c60c65af40b90f4998db3bddee469b0f0bd
AnnOl1 <60824081+AnnOl1@users.noreply.github.com>
2023-07-23 22:37:21 +0300

    main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-import asyncio # для асинхронного запуска бота
-import logging # для настройки логгирования, которое поможет в отладке
+import asyncio 
+import logging 
 
-from aiogram import Bot, Dispatcher # основной модуль библиотеки aiogram, из которого мы импортируем классы Bot и Dispatcher
-from aiogram.enums.parse_mode import ParseMode # содержит настройки разметки сообщений (HTML, Markdown)
-from aiogram.fsm.storage.memory import MemoryStorage # хранилища данных для состояний пользователей
+from aiogram import Bot, Dispatcher 
+from aiogram.enums.parse_mode import ParseMode 
+from aiogram.fsm.storage.memory import MemoryStorage 
 
-import config # настройки бота, пока что только токен
-from handlers import router # пока пустой, но скоро мы напишем в нём функционал нашего бота
+import config 
+from handlers import router 
 from aiogram.utils.chat_action import ChatActionMiddleware
 
 async def main():
